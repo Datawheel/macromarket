c1ed85e6235f9b5a2f137496aa972d7806d4a255
Sabrina Kantor <kantor.sa@husky.neu.edu>
2017-05-26 14:17:38 -0400

    commented out NotFound

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Route, IndexRoute} from "react-router";
-
 import App from "components/App";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
@@ -60,7 +59,7 @@ function checkForId(nextState, replace) {
     return replace({pathname: nextUrl});
   } else {
     // make sure it's legal
-    return <NotFound/>;
+    // return <NotFound/>;
   }
 }
 
