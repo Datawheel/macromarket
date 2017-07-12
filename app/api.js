3287f76dead08a94cd5b34fc4a1dcf2994071951
Alexander Simoes <alexandersimoes@gmail.com>
2017-07-12 17:23:10 -0400

    sets base url to https

--- app/api.js
+++ app/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 const clientConfig = {
-  host: process.env.MM_HOSTNAME || 'localhost',
-  port: process.env.MM_PORT || '8080'
+  host: process.env.MM_HOSTNAME || "localhost",
+  port: process.env.MM_PORT || "8080"
 };
 
 let baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
 if (process.env.NODE_ENV === "production") {
-  baseURL = `http://macro.market`;
+  baseURL = "https://macro.market";
 }
 export const url = baseURL;
 
-const axiosInstance = axios.create({ baseURL });
+const axiosInstance = axios.create({baseURL});
 
 export default axiosInstance;
