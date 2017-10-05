97c1ef993eb42144086f6dd34e1472b81a5564e4
Geoff House <house.g@gmail.com>
2017-10-05 12:28:48 -0700

    Country Page Layout
    Changed the design for the header section of the country page. I think
    this could be more of a template for the other pages so we could move
    away from the sidebar which is a little clunky.

--- app/components/CountryHeader.jsx
+++ app/components/CountryHeader.jsx
@@ -74,60 +74,70 @@ class CountryHeader extends React.Component {
       <div className="header-image-wrapper">
         <div className="fade-in background-image" style={{
           backgroundImage: `url(${placeImg})`
-        }}></div>
-        <Link to={"/settings/country"}>
-          <button className="list-company">List Your Company</button>
-        </Link>
-        <div className="image-overlay-wrapper">
-          <div className="image-overlay"></div>
-          <div className="text-wrapper">
-            <div className="name section-wrapper">
-              <h2>{country.name}</h2>
-              <h4>{country.continent}</h4>
+        }}><div className="image-overlay"></div>
+        </div>
+        <div className="header-info">
+
+          <div className="name page-title section-wrapper">
+            <div className="name-image">
               <img className="flag" src={`/images/flags/country_${country.id}.png`}></img>
             </div>
+            <div className="name-text">
+              <h2>{country.name}</h2>
+              <h4>{country.continent}</h4>
+            </div>
+          </div>
+          <div className="page-links">
+            <Link to={"/settings/country"}>
+              <button className="list-company">List Your Company</button>
+            </Link>
+            <a className="oec-link" href={`http://atlas.media.mit.edu/en/profile/country/${country.id_3char}`}>
+              View on the OEC <span className="chevron right"></span></a>
+          </div>
+        </div>
+        <div className="image-overlay-wrapper">
+
+          <div className="text-wrapper">
             <div className="section-wrapper continent-wrapper">
               <img src={`/images/flags/country_${continentId}.png`}></img>
             </div>
             {exportValue && importValue
               ? <div className="section-wrapper total-data">
-                  <div className="data">
-                    <img className="icon" src="/images/icons/icon-export.svg"></img>
-                    <p className="value">{exportValue}</p>
+                  <div className="data exports">
                     <h4>Exports</h4>
+                    <img className="icon" src="/images/icons/icon-export.svg"></img>
+                    <h3>{exportValue}</h3>
                   </div>
-                  <div className="data">
-                    <img className="icon" src="/images/icons/icon-import.svg"></img>
-                    <p className="value">{importValue}</p>
+                  <div className="data imports">
                     <h4 >Imports</h4>
+                    <img className="icon" src="/images/icons/icon-import.svg"></img>
+                    <h3>{importValue}</h3>
                   </div>
                 </div>
               : null}
             {exportName && importName
               ? <div className="section-wrapper top-data">
-                  <div className="data">
-                      <Link to={`/product/${exportData.hs92_id}`}>
-                    <img className="icon" src={`/images/product_icon/hs_${exportData.hs92_id.slice(0, 2)}.png`}></img>
+                  <div className="data top-export">
                     <h4>Top Export</h4>
-                    <div className="name-wrapper">
-                      <h4>{exportName}</h4>
-                    </div></Link>
+                    <img className="icon" src={`/images/product_icon/hs_${exportData.hs92_id.slice(0, 2)}.png`}></img>
+                    <Link to={`/product/${exportData.hs92_id}`}>
+                      <div className="name-wrapper">
+                        <h3>{exportName}</h3>
+                      </div>
+                    </Link>
                   </div>
 
-                  <div className="data">
-                      <Link to={`/product/${importData.hs92_id}`}>
-                    <img className="icon" src={`/images/product_icon/hs_${importData.hs92_id.slice(0, 2)}.png`}></img>
+                  <div className="data top-import">
                     <h4>Top Import</h4>
-                    <div className="name-wrapper">
-                      <h4>{importName}</h4>
-                    </div></Link>
+                    <img className="icon" src={`/images/product_icon/hs_${importData.hs92_id.slice(0, 2)}.png`}></img>
+                    <Link to={`/product/${importData.hs92_id}`}>
+                      <div className="name-wrapper">
+                        <h3>{importName}</h3>
+                      </div>
+                    </Link>
                   </div>
                 </div>
               : null}
-            <div className={countryData ? "link-wrapper section-wrapper" : "section-wrapper" }>
-              <a className="oec-link" href={`http://atlas.media.mit.edu/en/profile/country/${country.id_3char}`}>
-                View on the OEC <span className="chevron right"></span></a>
-            </div>
           </div>
         </div>
       </div>
