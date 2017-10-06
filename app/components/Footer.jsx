6a2107bc3be91ac42c9c92fd0a4da7da83c6f1e2
Alexander Simoes <alexandersimoes@gmail.com>
2017-10-06 14:50:28 -0400

    adds new site-wide links to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 import "./Footer.css";
 
-export default class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-
-    return (
-    <div className="footer">
-
-        <div className="logo-wrapper"><img src="/images/icons/logos/macro-market.svg" />
-       <img src="/images/icons/logos/white-market-logo.svg" /></div>
-        <div className="text-wrapper">
-        <p>The Observatory of Economic Complexity by Alexander Simoes is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
-        Permissions beyond the scope of this license may be available here.</p></div>
-      <div className="left">
+const Footer = () =>
+  <footer>
+    <div className="footer-links">
+      <div className="link-column">
+        <div className="title">Companies</div>
+        <a href="/company/1">Super Special Company 1</a>
+        <a href="/company/2">Super Special Company 2</a>
+        <a href="/company/3">Super Special Company 3</a>
+        <a href="/company/">View more...</a>
+      </div>
+      <div className="link-column">
+        <div className="title">Countries</div>
+        <a href="/country/sacol">Colombia</a>
+        <a href="/country/sabra">Brazil</a>
+        <a href="/country/aschn">China</a>
+        <a href="/country/asare">United Arab Emirites</a>
+        <a href="/country/">View more...</a>
+      </div>
+      <div className="link-column">
+        <div className="title">Products</div>
+        <a href="/product/115310">Jute Woven Fabric</a>
+        <a href="/product/063105">Mixed Mineral or Chemical Fertilizers</a>
+        <a href="/product/01030342">Tunas (yellowfin) frozen, whole</a>
+        <a href="/product/">View more...</a>
+      </div>
+      <div className="link-column">
+        <div className="title">Community</div>
+        <a href="https://connectamericas.com/" rel="noopener noreferrer" target="_blank">ConnectAmericas</a>
+        <a href="http://atlas.media.mit.edu/" rel="noopener noreferrer" target="_blank">Observatory of Economic Complexity</a>
+        <a href="https://www.media.mit.edu/groups/collective-learning/overview/" rel="noopener noreferrer" target="_blank">Collective Learning Group</a>
+        <a href="https://www.media.mit.edu/" rel="noopener noreferrer" target="_blank">MIT Media Lab</a>
+      </div>
+    </div>
+    <div className="footer-meta">
+      <div className="footer-copyright">
+      2017 Macro Market. <a href="#">Terms of service</a> and <a href="#">privacy policy</a> apply.
+      </div>
+      <div className="footer-logos">
         <img className="connect" src="/images/icons/logos/connectAmericasLogo.png" />
         <img className="collective-learning" src="/images/icons/logos/collective-learning.png" alt="Collective Learning Logo" />
         <img className="media-lab" src="/images/icons/logos/mit-media-lab.png" alt="MIT Media Lab Logo" />
         <img className="datawheel" src="/images/icons/logos/datawheel-sq.png" alt="Datawheel Logo" />
       </div>
     </div>
-    );
-  }
-}
+  </footer>;
+
+export default Footer;
