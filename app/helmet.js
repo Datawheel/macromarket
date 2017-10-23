f74aeb6951ae19f3fc0320744cebbe63d2396fe9
Alexander Simoes <alexandersimoes@gmail.com>
2017-10-23 17:39:54 -0400

    fixes mobile resizing by removing viewport restrictions

--- app/helmet.js
+++ app/helmet.js
@@ -6,7 +6,6 @@ module.exports = {
     {charset: "utf-8"},
     {"http-equiv": "X-UA-Compatible", "content": "IE=edge"},
     {name: "description", content: "Macro Market is a free service that allows people to post and find information about their business to facilitate the discovery of this information by potential business partners."},
-    {name: "viewport", content: "width=device-width, initial-scale=1"},
     {name: "mobile-web-app-capable", content: "yes"},
     {name: "apple-mobile-web-app-capable", content: "yes"},
     {name: "apple-mobile-web-app-status-bar-style", content: "black"},
