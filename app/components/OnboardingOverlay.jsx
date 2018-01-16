5e437f34f2575e6393656585388bd865a0a114e0
Nick Bond <bond.n@husky.neu.edu>
2018-01-16 13:11:21 -0500

    use global state to handle visibility of onboarding overlay

--- app/components/OnboardingOverlay.jsx
+++ app/components/OnboardingOverlay.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import {connect} from "react-redux";
 import OnboardingSlide from "./OnboardingSlide";
+import {toggleOverlay} from "../actions/onboardingActions";
 
 class OnboardingOverlay extends React.Component {
   constructor(props) {
     super(props);
-    const {source} = props.query;
-    const isVisible = source && source === "oec";
-    this.state = {isVisible};
+    const {source, toggleOverlay} = props.query;
+    if (source && source === "oec") {
+      toggleOverlay();
+    }
   }
 
   render() {
     return (
-      <div style={{display: this.state.isVisible ? "inline" : "none"}}>
-          <p>Sup Sabrina! :)</p>
-          return (<OnboardingSlide></OnboardingSlide>);
+      <div style={{display: this.props.isOpen ? "inline" : "none"}}>
+        <p>Sup Sabrina! :)</p>
+        <OnboardingSlide></OnboardingSlide>
       </div>
     );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
+  toggleOverlay: () => {
+    dispatch(toggleOverlay());
+  }
 });
 
-const mapStateToProps = state => ({product: state.data.product});
+const mapStateToProps = state => ({
+  product: state.data.product,
+  isOpen: state.onboarding.isOverlayOpen
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(OnboardingOverlay);
