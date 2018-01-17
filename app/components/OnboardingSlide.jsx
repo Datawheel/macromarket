b656cc9849ee0351758cfbf71e0bc21be81b9bb3
Nick Bond <bond.n@husky.neu.edu>
2018-01-17 12:17:28 -0500

    copy login and signup from datawheel-canon

--- app/components/OnboardingSlide.jsx
+++ app/components/OnboardingSlide.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Login as CanonLogin} from "datawheel-canon";
 import {SignUp} from "datawheel-canon";
+import {OnboardingSignUp} from "./OnboardingSignUp";
+import {OnboardingLogin} from "./OnboardingLogin";
 import {connect} from "react-redux";
 import EditProducts from "../pages/admin/EditProducts";
 import {CompanyDropdown} from "./CompanyDropdown";
@@ -16,19 +18,22 @@ class OnboardingSlide extends React.Component {
   }
 
   render() {
-    const slideNum = 5;
-    if (this.props.slideNumber === 10) {
+    const slideNum = 0;
+    const productName = "Bovine";
+    if (slideNum === 0) {
       return (
         <div className="slide">
           <div className="product-wrapper">
             <p>Interested in being listed under {this.props.product} ?</p>
           </div>
-          <button>Sign up</button>
-          <button>Login</button>
+          <button>Sign Up</button>
+          <button>Log In</button>
+          <OnboardingSignUp/>
+          <OnboardingLogin/>
         </div>
       );
     }
-    else if (this.props.slideNumber === 11) {
+    else if (slideNum === 1) {
       return (
         <div className="slide">
           <div className="product-wrapper">
@@ -37,7 +42,7 @@ class OnboardingSlide extends React.Component {
           <SignUp/>
         </div>);
     }
-    else if (this.props.slideNumber === 21) {
+    else if (slideNum === 2) {
       return (
         <div className="slide">
           <div className="product-wrapper">
@@ -45,22 +50,16 @@ class OnboardingSlide extends React.Component {
           </div>
         </div>);
     }
-    else if (this.props.slideNumber === 13) {
+    else if (slideNum === 3) {
       return (
         <div className="slide">
           <CanonLogin />
         </div>);
     }
-    else if (this.props.slideNumber === 14) {
+    else if (slideNum === 4) {
       return (
         <div className="slide">
-          <CanonLogin />
-        </div>);
-    }
-    else if (slideNum === 5) {
-      return (
-        <div className="slide">
-
+          <div className="company-dropdown"></div>
           <EditProducts/>
         </div>
       );
