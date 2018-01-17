58d14ce53f33f770137ba55431746fa1f4666f32
Helios <helios@Helioss-MacBook-Pro.local>
2018-01-17 11:42:02 -0500

    fixed product/country selection to work in form

--- app/components/OnboardingSlide.jsx
+++ app/components/OnboardingSlide.jsx
@@ -1,49 +1,73 @@
 import React from "react";
+import {Login as CanonLogin} from "datawheel-canon";
+import {SignUp} from "datawheel-canon";
 import {connect} from "react-redux";
-import OnboardingProducts from "./OnboardingProducts";
-import OnboardingGetStarted from "./OnboardingGetStarted";
-import OnboardingCompany from "./OnboardingCompany";
+import EditProducts from "../pages/admin/EditProducts";
+
 
 class OnboardingSlide extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    console.log(this.props);
+  }
 
   render() {
-    return (
-      <div className="slider-wrapper">
-        <div className="tabs">
-          <div className={this.props.slideNumber === 1 ? "active tab" : "tab"}><p>Company</p>
-            <div className="line"></div>
+    const slideNum = 4;
+    if (this.props.slideNumber === 0) {
+      return (
+        <div className="slide">
+          <div className="product-wrapper">
+            <p>Interested in being listed under {this.props.product} ?</p>
           </div>
-          <div className={this.props.slideNumber ===  2 ? "active tab" : "tab"}><p>Products</p>
-            <div className="line"></div>
-          </div>
+          <button>Sign up</button>
+          <button>Login</button>
         </div>
-        <button className="close" onClick={this.props.toggleOverlay}></button>
-        <div className={`slide-container slide-container-${this.props.slideNumber}`}>
-          <div className="slide slide-0">
-            <div className="logo-wrapper">
-              <img className="mm-logo" src="/images/icons/logos/macro-market.svg"></img>
-              <img src="/images/icons/logos/orange-market-logo.svg"></img>
-            </div>
-            <OnboardingGetStarted product={this.props.product}/>
+      );
+    }
+    else if (this.props.slideNumber === 1) {
+      return (
+        <div className="slide">
+          <div className="product-wrapper">
+            <p>Interested in being listed under {this.props.product} ?</p>
           </div>
-          <div className="slide slide-1" >
-            <OnboardingCompany/>
+          <SignUp/>
+        </div>);
+    }
+    else if (this.props.slideNumber === 2) {
+      return (
+        <div className="slide">
+          <div className="product-wrapper">
+            <p>Create a Company</p>
           </div>
-          <div className="slide slide-2">
-            <OnboardingProducts/>
-          </div>
+        </div>);
+    }
+    else if (this.props.slideNumber === 3) {
+      return (
+        <div className="slide">
+          <CanonLogin />
+        </div>);
+    }
+    else if (slideNum === 4) {
+      return (
+        <div className="slide">
+          <div className="company-dropdown"></div>
+          <EditProducts/>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return(<div></div>);
+    }
   }
 }
 
-const mapDispatchToProps = dispatch => ({});
 
-const mapStateToProps = state => ({products: state.data.products, user: state.auth.user});
+const mapDispatchToProps = dispatch => ({
+});
+
+const mapStateToProps = state => ({products: state.data.products});
 
 export default connect(mapStateToProps, mapDispatchToProps)(OnboardingSlide);
