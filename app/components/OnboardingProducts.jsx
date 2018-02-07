7b6037b8b99cfc642ca923f71e2cc2248f6e7cc1
Nick Bond <bond.n@husky.neu.edu>
2018-02-07 15:10:46 -0500

    pass required props to edit products form

--- app/components/OnboardingProducts.jsx
+++ app/components/OnboardingProducts.jsx
@@ -2,23 +2,46 @@ import React from "react";
 import {connect} from "react-redux";
 import EditProducts from "../pages/admin/EditProducts";
 import {setOnboardingCompany, updateSlideOverlay} from "../actions/onboardingActions";
+import api from "../api";
+
+async function getCompany(slug) {
+  const companyResponse = await api.get(`api/companies/${slug}`);
+  return companyResponse.data;
+}
 
 class OnboardingProducts extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      company: null
+    };
+  }
+
+  async componentDidMount() {
+    const {companySlug} = this.props;
+    const company = await getCompany(companySlug);
+    this.setState({company});
+  }
+
+  async componentWillReceiveProps(nextProps) {
+    if (nextProps.companySlug !== this.props.companySlug) {
+      const company = await getCompany(nextProps.companySlug);
+      this.setState({company});
+    }
   }
 
   render() {
+    const {company} = this.state;
     return (
       <div>
-        <EditProducts/>
+        <EditProducts onboardingCompany={company} companySlug={this.props.companySlug}/>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  company: state.onboarding.companyId,
+  companySlug: state.onboarding.companySlug,
   user: state.auth.user
 });
 
