a8456b3fb1eb2d0bbc49ec369a6349a07225ea81
Nick Bond <bond.n@husky.neu.edu>
2018-02-09 13:41:08 -0500

    make products form not prepopulate with existing products

--- app/components/OnboardingTradeEdit.jsx
+++ app/components/OnboardingTradeEdit.jsx
@@ -33,7 +33,7 @@ class TradeEdit extends React.Component {
         </td>
         <td className="trade-dest">
           {
-            trade.product && allCountries && trade.destinations
+            trade.product && allCountries
               ? <CountrySelection
                 countries={trade.destinations}
                 allCountries={allCountries}
@@ -44,7 +44,7 @@ class TradeEdit extends React.Component {
         </td>
         <td className="trade-origin">
           {
-            trade.product && allCountries && trade.origins
+            trade.product && allCountries
               ? <CountrySelection
                 countries={trade.origins}
                 allCountries={allCountries}
